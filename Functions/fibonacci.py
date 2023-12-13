613fbce197e1fe90fe83990b7d9974daeca97f6c
Fideltodayy <fidelotieno11@gmail.com>
2023-12-13 08:33:06 +0300

    Understood Fibonnacci with GatereMark 😎

---
+++ Functions/fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n ==2:
+        return[0,1]
+    else:
+        fib_sequence = [0,1]
+        while n > len(fib_sequence):
+            next_number = fib_sequence[-1] + fib_sequence[-2]
+            fib_sequence.append(next_number)
+            print(fib_sequence)
+
+fibonacci(10)
